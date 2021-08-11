15f54ed6076f997c6572677caf5b16239426d658
Coela Can't! <coelacannot@gmail.com>
2021-08-10 20:03:44 -0400

    Updated for Image class modifications

--- ImageConverter.py
+++ ImageConverter.py
@@ -1,26 +1,26 @@
 from PIL import Image
 import io
 
-inputFile = "Example Files\CoelaToot.png"
-outputFile = "Output\CoelaToot.h"
-name = "CoelaToot"
+inputFile = "Example Files\CoelaCantNoBG.png"
+outputFile = "Output\CoelaCant.h"
+name = "CoelaCant"
 
-def GetData():
-    image = Image.open(inputFile).convert('RGB')
+def GetData(className, file):
+    image = Image.open(file).convert('RGB')
     w, h = image.size
 
     data = "#pragma once\n\n"
     data += "#include \"..\..\Materials\Image.h\"\n\n"
-    data += "class " + name + " : public Image{\n"
+    data += "class " + className + " : public Image{\n"
     data += "private:\n"
     data += "\tstatic const uint8_t rgbMemory[];\n\n"
     data += "public:\n"
-    data += "\t" + name + "(Vector2D size, Vector2D offset) : Image(rgbMemory, " + str(w) + ", " + str(h) +") {\n"
+    data += "\t" + className + "(Vector2D size, Vector2D offset) : Image(Image::RGB, rgbMemory, " + str(w) + ", " + str(h) +") {\n"
     data += "\t\tSetSize(size);\n"
     data += "\t\tSetPosition(offset);\n"
     data += "\t}\n};\n\n"
 
-    data += "const uint8_t " + name + "::rgbMemory[] PROGMEM = {"
+    data += "const uint8_t " + className + "::rgbMemory[] PROGMEM = {"
     
     for i in range(h):
         for j in range(w):
@@ -36,7 +36,7 @@ def GetData():
     return data
 
 
-output = GetData()
+output = GetData(name, inputFile)
 
 print(output)
 f = open(outputFile, "w")
